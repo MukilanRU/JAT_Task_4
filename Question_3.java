7dc204517b0d3903f0f1313a716b676bdd9e0343
MukilanRU <102127014+MukilanRU@users.noreply.github.com>
2024-11-06 15:56:48 +0530

    Add files via upload

---
+++ Question_3.java
@@ -0,0 +1,16 @@
+package tasks.org;
+
+public class Question_3 {
+
+	//Question 3 :Write a program to print the characters from A to Z.
+
+	public static void main(String[] args) {
+		method3();
+	}
+	public static void method3() {
+		for (char i = 'A'; i <= 'Z'; i++) {
+			System.out.print(" "+i);
+		}
+	}
+
+}
