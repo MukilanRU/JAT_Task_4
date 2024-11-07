b197049d08b1ee0a7f2b78c707bad4ac6c16763c
MukilanRU <102127014+MukilanRU@users.noreply.github.com>
2024-11-07 15:34:04 +0530

    Add files via upload

---
+++ Question_7.java
@@ -0,0 +1,15 @@
+package tasks.org;
+
+public class Question_7 {
+
+	//Q7)Write a program to print the length of the given string. String msg="Guvi Geek"
+	public static void main(String[] args) {
+		method7();
+	}
+	public static void method7() {
+		String msg="Guvi Geek";
+		System.out.println("The String is \""+ msg+"\" and the length is "+msg.length());
+
+	}
+
+}
