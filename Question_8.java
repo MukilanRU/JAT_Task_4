dc8cf483380365e12e88e967158961c44d600104
MukilanRU <102127014+MukilanRU@users.noreply.github.com>
2024-11-07 15:38:58 +0530

    Add files via upload

---
+++ Question_8.java
@@ -0,0 +1,14 @@
+package tasks.org;
+
+public class Question_8 {
+	//Q8)Write a program To print "Welcome to Guvi" 10 times.
+	public static void main(String[] args) {
+		method8();
+	}
+	public static void method8() {
+		String a="Welcome to Guvi";
+		for (int i = 0; i <10; i++) {
+			System.out.println(a);
+		}
+	}
+}
